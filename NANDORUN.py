e5f24f7fb656591e6e20277f5cafdf30748bd147
Edward Digby <edwardjdigby@gmail.com>
2015-10-10 14:09:55 +0100

    Fixed errors

--- NANDORUN.py
+++ NANDORUN.py
@@ -1,19 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, render_template, request
+import requests
+
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def index():
-    return render_template("index.html", name=name)
+    return render_template("index.html")
 
-@app.route("/quiz", method="GET")
+@app.route("/quiz", methods="GET")
 def quiz():
-	return render_template("quiz.html", name=name)
+	return render_template("quiz.html")
 
-@app.route("/quiz", method="POST")
-def quiz():
+@app.route("/quiz", methods="POST")
+def quizanswers():
 	return ("hello world")
 
-if __name__ == "__main__": 
-   app.run(host='0.0.0.0', port=5000)
-
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
