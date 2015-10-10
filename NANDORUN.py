0f305b8f03d350ddd64055e562ed3a00cac79dac
Edward Digby <edwardjdigby@gmail.com>
2015-10-10 13:43:56 +0100

    Added templates

--- NANDORUN.py
+++ NANDORUN.py
@@ -2,11 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("")
+def index():
+    return render_template("index.html", name=name)
 
 
-if __name__ == "__main__":
-    app.run()
 
+if __name__ == "__main__": 
+   app.run(host='0.0.0.0', port=5000)
 
