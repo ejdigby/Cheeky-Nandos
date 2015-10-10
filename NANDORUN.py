fff0f158f618cb4fb1f289a697adac3fe0730d76
lizzie-hodnett <elizabeth.hodnett@bourne-grammar.lincs.sch.uk>
2015-10-10 14:01:27 +0100

    ertj

--- NANDORUN.py
+++ NANDORUN.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", name=name)
 
+@app.route("/quiz", method="GET")
+def quiz():
+	return render_template("quiz.html", name=name)
 
+@app.route("/quiz", method="POST")
+def quiz():
+	return ("hello world")
 
 if __name__ == "__main__": 
    app.run(host='0.0.0.0', port=5000)
+
 
